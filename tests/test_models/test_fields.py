a218eab362550323cc4d3dec08393c22b6683862
Zhuo Wei <zeaphoo@qq.com>
2020-02-13 14:53:40 +0800

    models/fields 100% coverage

--- tests/test_models/test_fields.py
+++ tests/test_models/test_fields.py
@@ -1,10 +1,28 @@
+import pytest
+from datetime import datetime, date, timedelta
+import json
+import uuid
 
 from tests.testmodels import (
     IntFieldsModel,
-
+    DataVersionFieldsModel,
+    DatetimeFieldsModel,
+    DateFieldsModel,
+    TimeDeltaFieldsModel,
+    JSONFieldsModel,
+    UUIDFieldsModel
 )
 
-def test_int_models():
+from postmodel.models.fields import (
+    CharField,
+    DecimalField,
+    DatetimeField,
+    UUIDField
+)
+
+from postmodel.exceptions import ConfigurationError
+
+def test_int_field():
     model = IntFieldsModel(id=1, intnum=10)
     assert model.intnum_null == None
     fields_map = model._meta.fields_map
@@ -17,3 +35,84 @@ def test_int_models():
     assert intnum_field.to_python_value(344) == 344
     assert intnum_field.to_python_value('876') == 876
     assert fields_map['intnum_null'].required == False
+
+def test_wrong_config():
+    with pytest.raises(ConfigurationError):
+        CharField(max_length = 0)
+    with pytest.raises(ConfigurationError):
+        DecimalField(max_digits = 0, decimal_places=10)
+    with pytest.raises(ConfigurationError):
+        DecimalField(max_digits = 128, decimal_places = -1)
+    with pytest.raises(ConfigurationError):
+        DatetimeField(auto_now=True, auto_now_add=True)
+
+def test_dataversion_field():
+    model = DataVersionFieldsModel(id=1)
+    assert model.data_ver == 0
+    fields_map = model._meta.fields_map
+    data_ver = fields_map['data_ver']
+    data_ver.auto_value(model)
+    assert model.data_ver == 1
+
+
+def test_datatime_field():
+    model = DatetimeFieldsModel(id=1, datetime=datetime.utcnow())
+    fields_map = model._meta.fields_map
+    field = fields_map['datetime']
+    fields_map['datetime_null'].auto_value(model)
+    assert model.datetime_null == None
+    assert field.to_python_value(None) == None
+    dt = datetime.now()
+    assert field.to_python_value(dt) == dt
+    assert field.to_python_value('2020-01-06 12:12:12') == datetime(2020, 1, 6, 12, 12, 12)
+    assert model.datetime_auto == None
+    assert model.datetime_add == None
+    dt = datetime.utcnow()
+    fields_map['datetime_auto'].auto_value(model)
+    fields_map['datetime_add'].auto_value(model)
+    assert model.datetime_auto > dt
+    assert model.datetime_add > dt
+
+def test_date_field():
+    model = DateFieldsModel(id=1, date=date.today())
+    fields_map = model._meta.fields_map
+    field = fields_map['date']
+    assert field.to_python_value(None) == None
+    dt = date.today()
+    assert field.to_python_value(dt) == dt
+    assert field.to_python_value('2020-01-06') == date(2020, 1, 6) 
+
+def test_timedelta_field():
+    model = TimeDeltaFieldsModel(id=1, timedelta=1000000)
+    fields_map = model._meta.fields_map
+    field = fields_map['timedelta']
+    assert field.to_python_value(None) == None
+    assert field.to_python_value(timedelta(20, 0, 0)) == timedelta(20, 0, 0)
+    assert field.to_python_value(1000) == timedelta(microseconds=1000)
+    assert field.to_db_value(None) == None
+    assert field.to_db_value(timedelta(days=1)) == 86400*1000000
+
+def test_json_field():
+    model = JSONFieldsModel(id=1, data={'fookey': 'hello', 'key2': 124})
+    fields_map = model._meta.fields_map
+    field = fields_map['data']
+    assert field.to_python_value(None) == None
+    assert field.to_python_value(['a', 'b']) == ['a', 'b']
+    assert field.to_python_value('["abc", 123, "cde"]') == ["abc", 123, "cde"]
+
+    assert field.to_db_value(None) == None
+    assert json.loads(field.to_db_value({'fookey': 'world', 'key2': 223})) == {'fookey': 'world', 'key2': 223}
+
+def test_uuid_field():
+    f = UUIDField(pk=True)
+    assert f.default == uuid.uuid4
+    model = UUIDFieldsModel(data='123e4567-e89b-12d3-a456-426655440000')
+    fields_map = model._meta.fields_map
+    field = fields_map['data']
+    v = uuid.uuid4()
+    assert field.to_python_value(None) == None
+    assert field.to_python_value(v) == uuid.UUID(str(v))
+    assert field.to_python_value(str(v)) == v
+
+    assert field.to_db_value(None) == None
+    assert field.to_db_value(v) == str(v)
